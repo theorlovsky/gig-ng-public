b866870e716e8554f1301e22ae680165ce79728e
Aleksei Kaida <alex94orlovsky@gmail.com>
2020-02-17 22:14:23 +0200

    setup project

---
+++ gig-ng-public/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
