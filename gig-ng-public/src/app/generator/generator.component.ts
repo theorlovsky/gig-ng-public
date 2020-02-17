b1deed393b74770baad322b9932928db473e2a57
Aleksei Kaida <alex94orlovsky@gmail.com>
2020-02-17 22:21:21 +0200

    setup generator route

---
+++ gig-ng-public/src/app/generator/generator.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generator',
+  templateUrl: './generator.component.html',
+  styleUrls: ['./generator.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GeneratorComponent {
+}
