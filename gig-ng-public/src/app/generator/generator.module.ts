de79953615677bc2526d82e51c15205bc3d8e947
Aleksei Kaida <alex94orlovsky@gmail.com>
2020-02-18 13:26:05 +0200

    implement shared CodeComponent

--- gig-ng-public/src/app/generator/generator.module.ts
+++ gig-ng-public/src/app/generator/generator.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { SharedModule } from '@app/shared/shared.module';
 import { GeneratorComponent } from './generator.component';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { GeneratorComponent } from './generator.component';
     FormsModule,
     MatInputModule,
     MatButtonModule,
+    SharedModule,
   ],
   declarations: [
     GeneratorComponent,
